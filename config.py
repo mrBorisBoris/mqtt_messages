c10a9c3517a679433b6398e4f2ef0665df34e312
Roman Suyazov-Domoratskij <ptichkass@mail.ru>
2022-11-12 17:46:26 +0300

    wrote ini file

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 import configparser
+
+config = configparser.ConfigParser()
+config['93.188.43.181'] = {}
+config['93.188.43.181']['port'] = str(8883)
+config['93.188.43.181']['user'] = 'client1'
+config['93.188.43.181']['password'] = 'aineekeechohdoo7haecah3r'
+print(config['93.188.43.181']['user'])
+with open('example.ini', 'w') as configfile:
+    config.write(configfile)
+print(config.sections())
