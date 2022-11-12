14eaa818a805ced10283b752cf8107cafb09bc91
Roman Suyazov-Domoratskij <ptichkass@mail.ru>
2022-11-12 18:52:25 +0300

    ini file renamed

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import configparser
+import os
+
+
+class Other:
+    def __init__(self, ini_path, ini_name):
+        self.ini_path = os.path.abspath('mqtt_messages/config.ini')
+        self.ini_name = 'config.ini'
+
 
 config = configparser.ConfigParser()
 config['93.188.43.181'] = {}
@@ -6,6 +14,18 @@ config['93.188.43.181']['port'] = str(8883)
 config['93.188.43.181']['user'] = 'client1'
 config['93.188.43.181']['password'] = 'aineekeechohdoo7haecah3r'
 print(config['93.188.43.181']['user'])
-with open('example.ini', 'w') as configfile:
+with open('config.ini', 'w') as configfile:
     config.write(configfile)
 print(config.sections())
+print(os.path.abspath('mqtt_messages/config.ini'))
+
+file_name = os.path.abspath('mqtt_messages/config.ini')
+other = Other(os.path.abspath('mqtt_messages/config.ini'), 'config.ini')
+print(other.ini_name)
+print(other.ini_path)
+
+
+
+
+
+
