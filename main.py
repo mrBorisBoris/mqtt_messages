b2c51b826da394e41a335fff48a6c7ae7a6cde56
Roman Suyazov <ptichkass@mail.ru>
2022-11-20 17:20:44 +0300

    all works with postgre

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import collections
 import time
 import paho.mqtt.client as mqtt
 import logging
+import postgre
 import codecs
 import config
 
@@ -22,44 +23,6 @@ logging.basicConfig(level=logging.INFO,
 
 logging.error('OSError: [Errno 51] Network is unreachable', exc_info=True)
 logging.error('--- Logging error ---', exc_info=True)
-
-
-def postgre_code(record):
-    try:
-        # Подключиться к существующей базе данных
-        connection = psycopg2.connect(user="psqlar",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password="Hgft667rD454w4e",
-                                      host="172.20.19.48",
-                                      port="5432",
-                                      database="postgres")
-
-        cursor = connection.cursor()
-        postgres_insert_query = """ INSERT INTO lpwan.mquery(topic, payload)
-                                           VALUES (%s,%s)"""
-        # LPWAN = 'Incotex/TEST'
-        record_to_insert = ('Incotex/TEST', str(record))
-
-        cursor.execute(postgres_insert_query, record_to_insert)
-
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Запись успешно добавлена ​​в таблицy")
-        logging.info('Запись успешно добавлена ​​в таблицy')
-
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-        logging.error("Ошибка при работе с PostgreSQL", error, exc_info=True)
-
-        postgre_code(record)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Соединение с PostgreSQL закрыто")
-            logging.info('Соединение с PostgreSQL закрыто')
-
-
 
 
 
@@ -85,16 +48,9 @@ def on_message(client, userdata, msg):
     logging.info(msg.topic)
     logging.info(msg.payload)
     queue_to.append(msg.payload)
-    data = str(msg.payload, 'UTF-8')
-
-    # print(type(data))
-    # print(data)
     record_to = data_mqtt.payload
     queue_to.popleft()
-
-    # print(data)
-    # print(type(data))
-    postgre_code(str(record_to))
+    postgre.postgre_code(str(record_to))
 
 
 queue_to = collections.deque()
@@ -114,5 +70,5 @@ client.connect("93.188.43.181", 8883)
 client.loop_forever()
 
 
-postgre_code(on_message)
+
 
