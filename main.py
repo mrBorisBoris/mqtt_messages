d2ef70bc52c40390bd72f8ed7e351f5e3dee66d7
Roman Suyazov <ptichkass@mail.ru>
2022-12-06 11:20:34 +0300

    delete some old files

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import threading
-import psycopg_test
-import MQTT_thread
+import MQTT_start
+import paho.mqtt.client as mqtt
+
+def start_one():
+    MQTT_starter = MQTT_start.MQTT(client=mqtt.Client(client_id="client1",
+                                                      clean_session=True,
+                                                      userdata=None,
+                                                      protocol=mqtt.MQTTv311,
+                                                      transport="tcp"))
+    MQTT_starter.MQTT_start()
 
 
-#class  Data_MQTT():
-#    """Общий класс входящей информации"""
-#    def __init__(self, topic, payload):
-#        self.topic = str(topic)
-#        self.payload = str(payload, 'UTF-8')
 
 
-psycopg = threading.Timer(3, psycopg_test.try_thread)
-mqtt = threading.Timer(1, MQTT_thread.start_one())
-
-
-mqtt.start()
-psycopg.start()
+thread_1 = threading.Thread(target=start_one(), args=())
+#psycopg = threading.Timer(3, psycopg_test.try_thread)
+#psycopg.start()
 
 
 
